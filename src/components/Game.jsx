f3568afb382b1ccf5a5db4c1df25738b5325b5a7
Inga <piavka91@gmail.com>
2023-01-10 18:03:26 -0300

    add prettier and eslint

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,45 +1,42 @@
-import React, { useState } from 'react';
-import Flashcard from './Flashcard';
-import './Game.scss';
+import React, { useState } from 'react'
+import Flashcard from './Flashcard'
+import './Game.scss'
 
 export default function Slider(props) {
-    const {words, id} = props;
-    const [index, setIndex] = useState(id);
-    
+    const { words, id } = props
+    const [index, setIndex] = useState(id)
 
-    const prevCard = () => {   
+    const prevCard = () => {
         if (index === 0) {
-            setIndex(words.length - 1);
+            setIndex(words.length - 1)
+        } else {
+            setIndex(index - 1)
         }
-        else {
-            setIndex(index - 1);
-        }
-    };
+    }
 
-    const nextCard = () => { 
+    const nextCard = () => {
         if (index === words.length - 1) {
-            setIndex(0);
+            setIndex(0)
+        } else {
+            setIndex(index + 1)
         }
-        else {
-            setIndex(index + 1);
-        }
-    };
-
-    // 
+    }
 
     return (
-        
-        <div >
+        <div>
             <h2>Нажмите на карточку, чтобы проверить перевод</h2>
-            <div className='cards'>
-            <Flashcard word={words[index].english} 
-                    transcription={words[index].transcription} 
-                    translation={words[index].russian}  style={{textAlign: 'center'}}></Flashcard>         
-            <div style={{textAlign: 'center'}}>
-                <button onClick={prevCard}>{'<-'}</button>
-                <button onClick={nextCard}>{'->'}</button>
-            </div>
+            <div className="cards">
+                <Flashcard
+                    word={words[index].english}
+                    transcription={words[index].transcription}
+                    translation={words[index].russian}
+                    style={{ textAlign: 'center' }}
+                ></Flashcard>
+                <div style={{ textAlign: 'center' }}>
+                    <button onClick={prevCard}>{'<-'}</button>
+                    <button onClick={nextCard}>{'->'}</button>
+                </div>
             </div>
         </div>
-    );
+    )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,28 @@
-import './Header.scss';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link
-  } from "react-router-dom";
-import Logo from '../assets/img/logo.png';
-import Logo2 from '../assets/img/logo2.png';
-
+import './Header.scss'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+// import Logo from '../assets/img/logo.png'
+import Logo2 from '../assets/img/logo2.png'
 
 function Header() {
     return (
-        <header className='header'>
-          <div className='container header__container'>
-          <Link to="/">
-            <img src={Logo2} alt="Logo" className='logo-pic'/>
-            </Link>
-            <nav className='header__nav'>
-          <ul className='header__list'>
-            <li className='header__link'>
-              <Link to="/">Home</Link>
-            </li>
-            <li className='header__link'>
-              <Link to="/game">Game</Link>
-            </li>
-            
-          </ul>
-        </nav>
-        </div>
+        <header className="header">
+            <div className="container header__container">
+                <Link to="/">
+                    <img src={Logo2} alt="Logo" className="logo-pic" />
+                </Link>
+                <nav className="header__nav">
+                    <ul className="header__list">
+                        <li className="header__link">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className="header__link">
+                            <Link to="/game">Game</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
-    );
+    )
 }
 
-export default Header;
+export default Header
